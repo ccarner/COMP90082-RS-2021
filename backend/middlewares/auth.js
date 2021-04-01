e15c4a012e286cd0e27d09f515f152cf976b38ec
ChaoWei <harry2300042@gmail.com>
2021-04-01 23:04:37 +0800

    remove unnecessary msg

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,11 +5,9 @@ const TOKEN_SECRET = config.get('jwtPrivateKey');
 module.exports = function(req, res, next) {
     const token = req.header('auth-token');
     
-    console.log(token)
     if(!token) return res.status(401).send('Access Denied');
 
     try{
-        console.log("verifying token");
         const verified = jwt.verify(token, TOKEN_SECRET)
         var mtoken = jwt.decode(token);
         console.log(mtoken);
