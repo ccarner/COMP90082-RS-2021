fde586a588e3ead01050d7c2033bb2aa016d01ae
ChaoWei <harry2300042@gmail.com>
2021-04-01 23:07:33 +0800

    [bugfix] cannot add a subject

--- backend/routes/subject.js
+++ backend/routes/subject.js
@@ -8,7 +8,32 @@ const {Subject} = require('../models/subject');
 
 
 // moderator creates a subject
-router.post('/add', auth, subjectController.addSubject)
+router.post('/add', auth, async (req, res) =>{
+
+
+    if(!req.user._moderator)
+        return res.status(403).send('you are not the moderator');
+
+    let subject = await Subject.findOne({ subject_code: req.body.subject_code.toUpperCase() });
+
+    if(subject)
+        return res.status(400).send("Subject already existed.");
+    
+    subject = new Subject({
+                        name : req.body.name,
+                        subject_code : req.body.subject_code.toUpperCase(),
+                        description : req.body.description,
+                        created_by : [req.user._id]
+                    });
+    
+    await subject.save();
+
+    return res.send(subject);
+
+});
+
+
+
 // moderator delete a subject
 router.delete('/delete', verify.verify, subjectController.deleteSubject)
 
--- backend/tests/integration/subject.test.js
+++ backend/tests/integration/subject.test.js
@@ -11,36 +11,53 @@ describe('/subject', () => {
         it('should return 200 if add subject is successful', async () => {
             const server = require('../../index');
 
-            const rootUser = new User({
-                account: "admin2",
-                password: "admin2",
-                student_number: 1111111,
-                is_moderator: true,
-                is_admin: true
-            });
+            const token = new User({ is_moderator: true,is_admin: true }).generateAuthToken();
 
-            const token = rootUser.generateAuthToken();
-            
-            console.log(token);
+            const res = await request(server)
+                        .post('/subject/add')
+                        .set('auth-token', token)
+                        .send({
+                            name : "SPM",
+                            subject_code : "swen90016",
+                            description : "fundamental course"
+                        });
 
-            
-            // const res = await request(server)
-            //             .post('/subject/add')
-            //             .set('x-auth-token', token)
-            //             .send({
-            //                 name : "SPM",
-            //                 subject_code : "swen90016",
-            //                 description : "fundamental course"
-            //             });
-
-            // expect(res.status).toBe(200);
-            // await Subject.remove({});
-            // await User.remove({});
-            // await server.close(); 
-
-
+           
+            expect(res.status).toBe(200);
+            await Subject.remove({});
+            await User.remove({});
+            await server.close(); 
         });
 
+        it('should return 400 if the subject has been existed', async () => {
+            const server = require('../../index');
+
+            const token = new User({ is_moderator: true,is_admin: true }).generateAuthToken();
+
+            await request(server)
+            .post('/subject/add')
+            .set('auth-token', token)
+            .send({
+                name : "SPM",
+                subject_code : "swen90016",
+                description : "fundamental course"
+            });
+
+            const res = await request(server)
+                        .post('/subject/add')
+                        .set('auth-token', token)
+                        .send({
+                            name : "SPM",
+                            subject_code : "swen90016",
+                            description : "fundamental course"
+                        });
+
+           
+            expect(res.status).toBe(400);
+            await Subject.remove({});
+            await User.remove({});
+            await server.close(); 
+        });
 
     });
 });
