0ef7fd7c0248b0dd9393f86f293bc3c2217f8597
hellodrf <r.du1@student.unimelb.edu.au>
2021-04-27 10:22:52 +0800

    Server admin api added

--- ServerAdminAPI/rest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('restart', views.restart, name='restart'),
-    path('info', views.info, name='info'),
-    path('rebuild', views.rebuild, name='rebuild'),
-]
