e5495009b63bf46c42a36593795b6c97c37f4ae7
Aaron Du <runfengd@student.unimelb.edu.au>
2021-04-28 01:45:13 +0000

    update installation scripts

--- backend/install.sh
+++ backend/install.sh
@@ -4,11 +4,11 @@ echo "1. Install npm pkg manager."
 sudo apt update && sudo apt -y install npm
 
 echo "2. Install npm dependencies."
-npm install
+sudo npm install
 
 echo "3. Install systemd service"
-sudo cp ../rsfront.service /lib/systemd/system
+sudo cp ../rsrepo.service /lib/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl start rsfront
+sudo systemctl start rsrepo
 
 echo "Done!"
