886309f17dea7e4e3a2732027e351d20061651e4
Aaron Du <58500164+hellodrf@users.noreply.github.com>
2021-04-28 09:33:32 +0800

    Update install.sh

--- ServerAdminAPI/install.sh
+++ ServerAdminAPI/install.sh
@@ -3,8 +3,8 @@
 echo "1. Install dependencies."
 sudo apt -y install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-python -m pip install Django
+python3 get-pip.py
+python3 -m pip install Django
 
 echo "2. Install systemd service"
 sudo cp ../rsadmin.service /lib/systemd/system
