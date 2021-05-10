15ba55ba72443658caf9543c1bb096be34b6a57f
hellodrf <r.du1@student.unimelb.edu.au>
2021-05-10 20:13:19 +0800

    nginx_front.conf fix

--- ServerAdminAPI/api/settings.py
+++ ServerAdminAPI/api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!g&*9f_5qw*lhtu@4z#tdh6b30@t-t@ug-+i!_cf26u4vl#*p@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["api.cervidae.com.au"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
