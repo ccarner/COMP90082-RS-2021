98cc34277a06c679714dbabc489af86c002aa994
hellodrf <r.du1@student.unimelb.edu.au>
2021-05-10 19:28:44 +0800

    fix nginx conf

--- Front_end/install.sh
+++ Front_end/install.sh
@@ -2,23 +2,26 @@
 
 echo "1. Install yarn pkg manager."
 sudo apt update && sudo apt -y install npm
-npm install --global yarn
+sudo npm install --global yarn
 
-echo "2. Install yarn dependencies & build frontend from source"
-yarn install
-yarn build
+echo "2. Enable swapfile"
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab >> /dev/null
 
-echo "3. Install nginx."
-sudo apt install nginx
+echo "3. Install yarn dependencies & build frontend from source"
+sudo yarn install
+sudo yarn build
+
+echo "4. Install nginx."
+sudo apt install -y nginx
 sudo cp nginx_front.conf /etc/nginx/sites-enabled
-mkdir --parents /var/www/front/public
-sudo cp ../build /var/www/front/public
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo mkdir --parents /var/www/front/public
+sudo cp -r build/* /var/www/front/public
 sudo chmod -R +r /var/www/front/public
 sudo systemctl restart nginx
-
-echo "4. Install systemd service"
-sudo cp ../rsfront.service /lib/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl start rsfront
 
 echo "Done!"
--- one-click-setup.sh
+++ one-click-setup.sh
@@ -7,10 +7,12 @@ echo "Configuring firewall."
 sudo iptables -I INPUT -p tcp --dport 8000 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 4000 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 
 sudo ufw allow 8000
 sudo ufw allow 4000
 sudo ufw allow 3000
+sudo ufw allow 80
 
 echo "A. Cloning from Github Source."
 sudo git clone https://github.com/ccarner/COMP90082-RS-2021.git
@@ -25,17 +27,19 @@ npm install --global yarn
 
 echo "C. Installing admin API."
 cd ServerAdminAPI
+sudo chmod +x install.sh
 sudo sh install.sh
 
 echo "D. Installing backend service."
 cd ../backend
+sudo chmod +x install.sh
 sudo sh install.sh
 
-#echo "E. Installing frontend service."
-#cd ../Front_end
-#sudo sh install.sh
+echo "E. Installing frontend service."
+cd ../Front_end
+sudo chmod +x install.sh
+sudo sh install.sh
 
 echo "Done!"
 sudo systemctl status rsadmin
 sudo systemctl status rsrepo
-#sudo systemctl status rsfront
