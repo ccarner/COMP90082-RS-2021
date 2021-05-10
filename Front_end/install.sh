7c6133d3a100090a6237e6f63371dc02ade56a99
hellodrf <r.du1@student.unimelb.edu.au>
2021-05-10 18:31:42 +0800

    add front-end prod deployment

--- Front_end/install.sh
+++ Front_end/install.sh
@@ -4,10 +4,19 @@ echo "1. Install yarn pkg manager."
 sudo apt update && sudo apt -y install npm
 npm install --global yarn
 
-echo "2. Install yarn dependencies."
+echo "2. Install yarn dependencies & build frontend from source"
 yarn install
+yarn build
 
-echo "3. Install systemd service"
+echo "3. Install nginx."
+sudo apt install nginx
+sudo cp nginx_front.conf /etc/nginx/sites-enabled
+mkdir --parents /var/www/front/public
+sudo cp ../build /var/www/front/public
+sudo chmod -R +r /var/www/front/public
+sudo systemctl restart nginx
+
+echo "4. Install systemd service"
 sudo cp ../rsfront.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start rsfront
