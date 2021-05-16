d58695bcec33e57de466b71a1c93c2f3e6c4e13a
hellodrf <r.du1@student.unimelb.edu.au>
2021-05-16 13:20:05 +0800

    fix firewall issue

--- one-click-setup.sh
+++ one-click-setup.sh
@@ -8,12 +8,17 @@ sudo iptables -I INPUT -p tcp --dport 8000 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 4000 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+echo "#\!/bin/bash\niptables-restore < /etc/iptables.rules" > /etc/network/if-pre-up.d/iptables
+iptables-save | sudo tee -a /etc/iptables.rules >> /dev/null
+chmod  +x /etc/network/if-pre-up.d/iptables
 
+sudo ufw enable
 sudo ufw allow 8000
 sudo ufw allow 4000
 sudo ufw allow 3000
 sudo ufw allow 80
 
+printf "\n\n\n"
 echo "A. Cloning from Github Source."
 sudo git clone https://github.com/ccarner/COMP90082-RS-2021.git
 sudo mv COMP90082-RS-2021 node
@@ -21,25 +26,32 @@ cd node|| exit
 sudo git checkout backend/develop
 
 
+printf "\n\n\n"
 echo "B. Installing dependencies."
 sudo apt update && sudo apt -y install npm && sudo apt -y install python3
 npm install --global yarn
 
+printf "\n\n\n"
 echo "C. Installing admin API."
 cd ServerAdminAPI|| exit
 sudo chmod +x install.sh
 sudo sh install.sh
 
+printf "\n\n\n"
 echo "D. Installing backend service."
 cd ../backend|| exit
 sudo chmod +x install.sh
 sudo sh install.sh
 
+printf "\n\n\n"
 echo "E. Installing frontend service."
 cd ../Front_end|| exit
 sudo chmod +x install.sh
 sudo sh install.sh
 
-echo "Done!"
-sudo systemctl status rsadmin
-sudo systemctl status rsrepo
+echo "All Done!"
+sudo systemctl status rsadmin | cat
+printf "\n\n\n"
+sudo systemctl status rsrepo | cat
+printf "\n\n\n"
+sudo systemctl status nginx | cat
