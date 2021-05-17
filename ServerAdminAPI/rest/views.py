89cdc3527c84ca2124ee6a731287c443dda4addb
hellodrf <r.du1@student.unimelb.edu.au>
2021-05-17 16:24:22 +0800

    bash patch

--- ServerAdminAPI/rest/views.py
+++ ServerAdminAPI/rest/views.py
@@ -23,16 +23,13 @@ def check(request):
 
 def rebuild(request):
     bash = "cd /var/node && git fetch origin && git checkout --quiet origin/backend/develop"
+    bash2 = "cd /var/node/backend && npm install"
+    bash3 = "cd /var/node/Front_end && yarn install && yarn build && sudo cp -r build/* /var/www/front/public"
     result = subprocess.check_output(bash, shell=True)
+    result += subprocess.check_output(bash2, shell=True)
+    result += subprocess.check_output(bash3, shell=True)
+    result = b"Rebuild succeeded.\n\n" + result + b"\n\n" + restart(request).content
 
-    if b'Already up to date.\n' not in result:
-        bash2 = "cd /var/node/backend && npm install"
-        bash3 = "cd /var/node/Front_end && yarn install && yarn build && sudo cp -r build/* /var/www/front/public"
-        result += subprocess.check_output(bash2, shell=True)
-        result += subprocess.check_output(bash3, shell=True)
-        result = b"Rebuild succeeded.\n\n" + result + b"\n\n" + restart(request).content
-    else:
-        result = result + b"\n\n" + info(request).content
     return HttpResponse(result, content_type="text/plain; charset=utf-8")
 
 
