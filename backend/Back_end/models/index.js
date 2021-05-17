59de311afeaa0515a4c1dcac71a7f169f4f7d18c
2635215324 <2635215324@qq.com>
2021-05-17 19:02:31 +0800

    modify cache

--- backend/Back_end/models/index.js
+++ backend/Back_end/models/index.js
@@ -22,64 +22,6 @@ db.once('open',function(){
 });
 
 
-// cache.js
-const redis = require('redis');
-const config = require('config');
-const logger = require('winston');
-
-const redisObj = {
-    client: null,
-    connect: function () {
-        this.client = redis.createClient(config.redis);
-        this.client.on('error', function (err) {
-            logger.error('redisCache Error ' + err);
-        });
-        this.client.on('ready', function () {
-            logger.info('redisCache connection succeed');
-        });
-    },
-    init: function () {
-        this.connect();
-        const instance = this.client;
-
-
-        const get = instance.get;
-        const set = instance.set;
-        const setex = instance.setex;
-
-        instance.set = function (key, value, callback) {
-            if (value !== undefined) {
-                set.call(instance, key, JSON.stringify(value), callback);
-            }
-        };
-
-        instance.get = function (key, callback) {
-
-            get.call(instance, key, (err, val) => {
-                if (err) {
-                    logger.warn('redis.get: ', key, err);
-                }
-                callback(null, JSON.parse(val));
-            });
-
-        };
-
-        instance.setex = function (key, value, callback) {
-            if (value !== undefined) {
-                setex.call(instance, key, config.cache.maxAge, JSON.stringify(value), callback);
-            }
-        };
-
-        return instance;
-
-    },
-};
-
-
-module.exports = redisObj.init();
-
-
-
   // models
 require('./user');
 require('./subject');
