0ec840894111c0e99686140a4be2115c556fff9f
hellodrf <r.du1@student.unimelb.edu.au>
2021-05-17 16:22:16 +0800

    fix git fetch issue

--- ServerAdminAPI/rest/views.py
+++ ServerAdminAPI/rest/views.py
@@ -22,7 +22,7 @@ def check(request):
         return HttpResponse(0, content_type="text/plain; charset=utf-8")
 
 def rebuild(request):
-    bash = "cd /var/node && git stash && git pull --no-edit --quiet"
+    bash = "cd /var/node && git fetch origin && git checkout --quiet origin/backend/develop"
     result = subprocess.check_output(bash, shell=True)
 
     if b'Already up to date.\n' not in result:
