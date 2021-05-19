835dde857edcb6915315e670bb0c256aede9c060
Aaron Du <aaronduooo@gmail.com>
2021-05-19 22:39:39 +0800

    update auto install script

--- one-click-setup.sh
+++ one-click-setup.sh
@@ -4,54 +4,59 @@ rm -rf /var/node
 cd /var || return
 
 echo "Configuring firewall."
-sudo iptables -I INPUT -p tcp --dport 8000 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 4000 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8000 -j ACCEPT
+iptables -I INPUT -p tcp --dport 4000 -j ACCEPT
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -p tcp --dport 27017 -j ACCEPT
+
+mkdir -p /etc/network/if-pre-up.d/iptables
 echo "#\!/bin/bash\niptables-restore < /etc/iptables.rules" > /etc/network/if-pre-up.d/iptables
 iptables-save | sudo tee -a /etc/iptables.rules >> /dev/null
 chmod  +x /etc/network/if-pre-up.d/iptables
 
-sudo ufw enable
-sudo ufw allow 8000
-sudo ufw allow 4000
-sudo ufw allow 3000
-sudo ufw allow 80
+ufw enable
+ufw allow 8000
+ufw allow 4000
+ufw allow 80
+ufw allow 27017
+
+ulimit -n 64000 && ulimit -u 64000
 
 printf "\n\n\n"
 echo "A. Cloning from Github Source."
-sudo git clone https://github.com/ccarner/COMP90082-RS-2021.git
-sudo mv COMP90082-RS-2021 node
+git clone https://github.com/ccarner/COMP90082-RS-2021.git
+mv COMP90082-RS-2021 node
 cd node|| exit
-sudo git checkout backend/develop
+git checkout backend/develop
 
 
 printf "\n\n\n"
 echo "B. Installing dependencies."
-sudo apt update && sudo apt -y install npm && sudo apt -y install python3
+apt update && apt -y install npm && apt -y install python3
 npm install --global yarn
 
 printf "\n\n\n"
 echo "C. Installing admin API."
 cd ServerAdminAPI|| exit
-sudo chmod +x install.sh
-sudo sh install.sh
+chmod +x install.sh
+sh install.sh
 
 printf "\n\n\n"
 echo "D. Installing backend service."
 cd ../backend|| exit
-sudo chmod +x install.sh
-sudo sh install.sh
+chmod +x install.sh
+sh install.sh
 
 printf "\n\n\n"
 echo "E. Installing frontend service."
 cd ../Front_end|| exit
-sudo chmod +x install.sh
-sudo sh install.sh
+chmod +x install.sh
+sh install.sh
 
+printf "\n\n\n"
 echo "All Done!"
-sudo systemctl status rsadmin | cat
-printf "\n\n\n"
-sudo systemctl status rsrepo | cat
-printf "\n\n\n"
-sudo systemctl status nginx | cat
+systemctl status rsadmin | cat
+printf "\n\n"
+systemctl status rsrepo | cat
+printf "\n\n"
+systemctl status nginx | cat
