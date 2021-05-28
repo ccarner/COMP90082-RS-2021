b3edf9f836c4b1aa34f069e85d0495510eb5014d
Aaron Du <aaronduooo@gmail.com>
2021-05-28 21:36:31 +0800

    update install.sh

--- Front_end/install.sh
+++ Front_end/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
+
+CERT=$1
+
 echo "1. Install yarn pkg manager."
 sudo apt update && sudo apt -y install npm
 sudo npm install --global yarn
@@ -11,7 +14,14 @@ sudo yarn build
 echo "3. Install nginx."
 sudo apt install -y nginx
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo cp default /etc/nginx/sites-enabled
+
+if [ $CERT = true ]; then
+  sudo cp default /etc/nginx/sites-enabled
+else
+  sudo cp default_ssl /etc/nginx/sites-enabled/
+  sudo mv /etc/nginx/sites-enabled/default_ssl /etc/nginx/sites-enabled/default
+fi
+
 sudo mkdir --parents /var/www/front/public
 sudo cp -r build/* /var/www/front/public
 sudo chmod -R +r /var/www/front/public
